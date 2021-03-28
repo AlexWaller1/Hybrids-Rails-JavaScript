4cf80a86d673597430c122f6146a6f8ab8333b8d
Alex Waller <alex@artofalexwaller.com>
2021-03-28 00:10:51 -0400

    hybrid

---
+++ frontend/src/index.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createForm();
+    fetchUsers()
+})
+
+const BASE_URL = "http://localhost:3000"
+
+// read - fetch users
+// will manipulate DOM to list users
+
+function fetchUsers(){
+    fetch(`${BASE_URL}/users`)
+    .then(resp => resp.json())
+    .then(users => {
+       // .then is where we do something with the data fetched
+        for (const user of users){
+            // current have rails objects
+            let u = new User(user.id, user.name, user.username, user.email)
+            // new javascript object created
+            u.renderUser()
+        }
+    })
+   
+}
+
+
+
+// create - create a new user
+
+// create a form
+// add an event listener
+// once form is submitted => fetch `post` to my back end
+// do something with the returned object
+
+function createForm(){
+    let usersForm = document.getElementById("users-form")
+
+    usersForm.innerHTML +=
+    `
+    <form>
+    Name: <input type="text" id="name"><br>
+    Username: <input type="text" id="username"><br>
+    Email: <input type="text" id="email"><br>
+    <input type="submit" value="Create User">
+    </form>
+    `
+
+   usersForm.addEventListener("submit", userFormSubmission)
+}
+
+function userFormSubmission(e){
+    e.preventDefault(); 
+    let name = document.getElementById("name").value
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
+
+    let user = {
+        name: name,
+        username: username,
+        email: email
+    }
+
+    fetch(`${BASE_URL}/users`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(resp => resp.json())
+    .then(user => {
+        let u = new User(user.id, user.name, user.username, user.email)
+        u.renderUser();
+    })
+}
+
+
+
+// delete - delete a user
+
+function deleteUser(e){
+    let userId = parseInt(e.target.dataset.id)
+
+    fetch(`${BASE_URL}/users/${userId}`, {
+        method: 'DELETE'
+    })
+
+    this.location.reload()
+}
