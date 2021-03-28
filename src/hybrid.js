9b6f4d235bc2b95f771815856aece87f6f1a174d
Alex Waller <alex@artofalexwaller.com>
2021-03-28 18:43:58 -0400

    render hybrid

--- src/hybrid.js
+++ src/hybrid.js
@@ -6,4 +6,16 @@ class Hybrid{
 
     // instance method thats going to render object to the dom
     // whatever behavior belongs to instance or class is stored in here.
+    renderHybrid() {
+        let hybridsDiv = document.getElementById("hybrids-container")
+
+        hybridsDiv.innerHTML +=
+
+        `
+        <ul>
+        <img src=${this.image}, alt="Hybrid Image">
+        <li> Description: ${this.caption} </li>
+        </ul>
+        `
+    }
 }
