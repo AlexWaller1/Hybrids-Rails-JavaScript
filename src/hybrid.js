7f19a33fad317ab547be36e6d4641c677ac15973
Alex Waller <alex@artofalexwaller.com>
2021-03-28 17:00:20 -0400

    post fetch

---
+++ src/hybrid.js
@@ -0,0 +1,9 @@
+class Hybrid{
+    constructor(img_src, caption){
+        this.img_src = img_src;
+        this.caption = caption;
+    }
+
+    // instance method thats going to render object to the dom
+    // whatever behavior belongs to instance or class is stored in here.
+}
